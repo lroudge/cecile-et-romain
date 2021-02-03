3797ac2d8e7e4247bffee8bdd46950134616ae0a
Laura Roudge <laura.derohan@gmail.com>
2021-02-03 17:33:44 +0100

    try with main

--- src/next.config.js
+++ src/next.config.js
@@ -2,6 +2,6 @@ const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
 	assetPrefix: isProd
-		? "https://cdn.statically.io/gh/lroudge/lroudge.github.io/github_page/"
+		? "https://cdn.statically.io/gh/lroudge/lroudge.github.io/main/"
 		: ""
 };
