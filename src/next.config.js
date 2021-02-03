a99125a0abe7b8bf31840fcfce3078ba9d091082
Laura Roudge <laura.derohan@gmail.com>
2021-02-03 17:35:53 +0100

    Without next config

--- src/next.config.js
+++ src/next.config.js
@@ -1,7 +1,7 @@
-const isProd = process.env.NODE_ENV === "production";
+// const isProd = process.env.NODE_ENV === "production";
 
-module.exports = {
-	assetPrefix: isProd
-		? "https://cdn.statically.io/gh/lroudge/lroudge.github.io/main/"
-		: ""
-};
+// module.exports = {
+// 	assetPrefix: isProd
+// 		? "https://cdn.statically.io/gh/lroudge/lroudge.github.io/main/"
+// 		: ""
+// };
