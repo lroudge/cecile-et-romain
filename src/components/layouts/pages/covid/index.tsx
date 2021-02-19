bcd1b688f79324d59862bb0cc952e4d9bd35333b
Laura Roudge <laura.derohan@gmail.com>
2021-02-19 13:17:10 +0100

    Add placeholders other than lorem ipsum

--- src/components/layouts/pages/covid/index.tsx
+++ src/components/layouts/pages/covid/index.tsx
@@ -2,20 +2,7 @@ export const Covid = () => {
 	return (
 		<div>
 			<h1>Infos Covid-19</h1>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a
-				tristique nisl. Aenean eget sapien nec lorem tincidunt lobortis
-				vitae et dui. Etiam justo mi, maximus sed feugiat id, vestibulum
-				in mi. In ultrices dui a nulla semper varius. Phasellus
-				pulvinar, justo vitae ultrices bibendum, purus nunc hendrerit
-				dui, non dictum leo lorem et enim. Suspendisse blandit pharetra
-				lobortis. Ut feugiat, nisl a malesuada tincidunt, felis mi
-				ornare risus, at pharetra mi neque eget nibh. Vestibulum ante
-				ipsum primis in faucibus orci luctus et ultrices posuere cubilia
-				curae; Proin molestie fringilla purus, ut bibendum massa porta
-				vel. Nam fringilla maximus egestas. Maecenas malesuada semper
-				leo, quis molestie augue rhoncus in. Morbi et euismod magna.
-			</p>
+			<h2>Infos à venir.</h2>
 		</div>
 	);
 };
--- src/components/layouts/pages/gallery/index.tsx
+++ src/components/layouts/pages/gallery/index.tsx
@@ -2,20 +2,10 @@ export const Gallery = () => {
 	return (
 		<div>
 			<h1>Photos</h1>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a
-				tristique nisl. Aenean eget sapien nec lorem tincidunt lobortis
-				vitae et dui. Etiam justo mi, maximus sed feugiat id, vestibulum
-				in mi. In ultrices dui a nulla semper varius. Phasellus
-				pulvinar, justo vitae ultrices bibendum, purus nunc hendrerit
-				dui, non dictum leo lorem et enim. Suspendisse blandit pharetra
-				lobortis. Ut feugiat, nisl a malesuada tincidunt, felis mi
-				ornare risus, at pharetra mi neque eget nibh. Vestibulum ante
-				ipsum primis in faucibus orci luctus et ultrices posuere cubilia
-				curae; Proin molestie fringilla purus, ut bibendum massa porta
-				vel. Nam fringilla maximus egestas. Maecenas malesuada semper
-				leo, quis molestie augue rhoncus in. Morbi et euismod magna.
-			</p>
+			<h2>
+				Revenez ici après le mariage pour découvrir les photos du
+				week-end !
+			</h2>
 		</div>
 	);
 };
