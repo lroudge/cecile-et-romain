76e527d550c659601a27d9914107f29c77487475
Laura Roudge <laura.derohan@gmail.com>
2021-02-25 19:26:23 +0100

    Next-18next stabilized

--- src/next.config.js
+++ src/next.config.js
@@ -1,9 +1,8 @@
+const { i18n } = require("./next-i18next.config");
+
 module.exports = {
 	env: {
 		GOOGLE_API_TOKEN: process.env.GOOGLE_API_TOKEN
 	},
-	i18n: {
-		defaultLocale: 'fr',
-		locales: ['fr', 'en']
-	}
+	i18n
 };
