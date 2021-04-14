d2e1b65734671e5618f8ccf5f341bcc082daf588
Laura Roudge <laura.derohan@gmail.com>
2021-04-14 16:18:17 +0200

    Add Security Headers

--- src/next.config.js
+++ src/next.config.js
@@ -1,6 +1,58 @@
 const { i18n } = require("./next-i18next.config");
 
+const ContentSecurityPolicy = `
+	default-src 'self';
+	script-src 'self' 'unsafe-eval' 'unsafe-inline' *.google.com;
+	child-src *.google.com;
+	style-src 'self' 'unsafe-inline';
+	font-src 'self';
+	img-src 'self' *.githubusercontent.com;
+`;
+
+const securityHeaders = [
+	{
+		key: "Content-Security-Policy",
+		value: ContentSecurityPolicy.replace(/\n/g, "")
+	},
+	{
+		key: "Referrer-Policy",
+		value: "origin-when-cross-origin"
+	},
+	{
+		key: "X-Frame-Options",
+		value: "DENY"
+	},
+	{
+		key: "X-Content-Type-Options",
+		value: "nosniff"
+	},
+	{
+		key: "X-DNS-Prefetch-Control",
+		value: "on"
+	},
+	{
+		key: "Strict-Transport-Security",
+		value: "max-age=315360000; includeSubDomains; preload"
+	},
+	{
+		key: "Permissions-Policy",
+		value: "camera=(), microphone=(), geolocation=()"
+	}
+];
+
 module.exports = {
+	async headers() {
+		return [
+			{
+				source: "/",
+				headers: securityHeaders
+			},
+			{
+				source: "/:path*",
+				headers: securityHeaders
+			}
+		];
+	},
 	env: {
 		GOOGLE_API_TOKEN: process.env.GOOGLE_API_TOKEN
 	},
